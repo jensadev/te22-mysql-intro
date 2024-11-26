0c447c7709f455e04bfac54e3f511a3f418472f6
Jens Andreasson <jens.andreasson@ntig.se>
2024-11-26 09:17:11 +0100

    Add bird retrieval endpoints and refactor query logic in server.js

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-import 'dotenv/config'
-import express from 'express'
-import pool  from './db.js'
+import "dotenv/config"
+import express from "express"
+import pool from "./db.js"
 
 const app = express()
 const port = 3000
 
-app.get('/', async (req, res) => {
-  const [birds] = await pool.promise().query('SELECT * FROM birds')
-
+app.get("/birds", async (req, res) => {
+  // const [birds] = await pool.promise().query('SELECT * FROM birds')
+  const [birds] = await pool
+    .promise()
+    .query(
+      "SELECT birds.*, species.name AS species FROM birds JOIN species ON birds.species_id = species.id;",
+    )
   res.json(birds)
 })
 
+app.get("/birds/:id", async (req, res) => {
+  const [bird] = await pool
+    .promise()
+    .query(
+      "SELECT birds.*, species.name AS species FROM birds JOIN species ON birds.species_id = species.id WHERE birds.id = ?;",
+      [req.params.id],
+    )
+  res.json(bird[0])
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
